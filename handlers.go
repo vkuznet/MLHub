a25c5a4af246fb892072bc7570820e461fc1282a
Valentin Kuznetsov <vkuznet@gmail.com>
2023-04-28 15:52:38 -0400

    R&D for r.Body

--- handlers.go
+++ handlers.go
@@ -6,12 +6,14 @@ package main
 //
 
 import (
+	"bytes"
 	"compress/gzip"
 	"encoding/json"
 	"errors"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"log"
+	"mime/multipart"
 	"net/http"
 	"strings"
 	"time"
@@ -99,6 +101,9 @@ func FaviconHandler(w http.ResponseWriter, r *http.Request) {
 func PredictHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	if model, ok := vars["model"]; ok {
+		if Config.Verbose > 0 {
+			log.Printf("request predictions from ML model %s", model)
+		}
 		// get ML meta-data
 		spec := bson.M{"model": model}
 		records, err := MongoGet(Config.DBName, Config.DBColl, spec, 0, -1)
@@ -114,15 +119,104 @@ func PredictHandler(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 		rec := records[0]
+		if Config.Verbose > 0 {
+			log.Printf("Found ML MetaData record %+v", rec)
+		}
+		log.Printf("ML backends %+v", Config.MLBackends)
 		if backend, ok := Config.MLBackends[rec.Type]; ok {
-			uri := fmt.Sprintf("%s/%s", backend.URI, backend.PredictApi)
+			path := r.RequestURI
+			bPath := strings.Replace(path, fmt.Sprintf("/model/%s", model), "", -1)
+			uri := fmt.Sprintf("%s", backend.URI)
 			if Config.Verbose > 0 {
-				log.Printf("request predictions from ML model %s at %s", model, uri)
+				log.Printf("request predictions from ML model %s at %s get %s", model, uri, bPath)
 			}
+			r.RequestURI = bPath
+			r.URL.Path = bPath
+			// TODO: I need to adjust r.Body to add model=mnist form value
+			//             if backend.Type == "TensorFlow" {
+			//                 updateRequestForm(r, "model", model)
+			//             }
 			reverseProxy(uri, w, r)
 		}
+		return
 	}
 	httpError(w, r, BadRequest, errors.New("no model name is provided"), http.StatusBadRequest)
+}
+
+func updateRequestForm(r *http.Request, key, value string) {
+	ctype := r.Header.Get("Content-Type")
+	data, err := io.ReadAll(r.Body)
+	if err == nil {
+		log.Printf("### data\n%s\nsize=%d", string(data), len(data))
+	}
+	r.Body.Close()
+	boundary := strings.Split(string(data), "\n")[0]
+	body := fmt.Sprintf("%s\nContent-Disposition: form-data; name=\"%s\"\n\n%s\n", boundary, key, value)
+	body += boundary
+	body += string(data)
+	r.Body = io.NopCloser(strings.NewReader(body))
+	r.Header.Set("Content-Length", fmt.Sprintf("%d", len([]byte(body))))
+	r.Header.Set("Content-Type", ctype)
+	r.ContentLength = int64(len([]byte(body)))
+	log.Printf("+++ data\n%s\nsize=%d", body, len([]byte(body)))
+
+	/*
+		err := r.ParseMultipartForm(32 << 20) // maxMemory
+		log.Printf("### data %+v, error %v", r.MultipartForm, err, r.Header.Get("Content-Length"))
+		// ### data &{Value:map[] File:map[image:[0xc000180240]]}, error <nil>%!(EXTRA string=831)
+		r.MultipartForm.Value[key] = []string{value}
+		log.Printf("### data %+v, error %v", r.MultipartForm, err, r.Header.Get("Content-Length"))
+
+		// write multipart form to body
+		var buf bytes.Buffer
+
+		// create new multipart writer
+		w := multipart.NewWriter(&buf)
+		defer w.Close()
+
+		// create new field
+		for k, v := range r.MultipartForm.Value {
+			w.WriteField(k, v)
+		}
+		for k, v := range r.MultipartForm.File {
+		}
+
+		// update conten type
+		r.Header.Set("Content-Type", w.FormDataContentType())
+		r.Body = io.NopCloser(strings.NewReader(string(buf.Bytes())))
+		r.Header.Set("Content-Length", fmt.Sprintf("%d", buf.Len()))
+	*/
+}
+func updateRequestForm2(r *http.Request, key, value string) {
+
+	var buf bytes.Buffer
+	var err error
+	var data []byte
+
+	// read existing body content
+	//     defer r.Body.Close()
+	//     data, err = io.ReadAll(r.Body)
+	//     if err == nil {
+	//         b := bytes.NewBuffer(data)
+	//         buf = *b
+	//     }
+	//     log.Println("### data", string(data), buf.Len())
+
+	// create new multipart writer
+	w := multipart.NewWriter(&buf)
+	defer w.Close()
+
+	// create new field
+	w.WriteField(key, value)
+
+	// update conten type
+	r.Header.Set("Content-Type", w.FormDataContentType())
+	r.Body = io.NopCloser(strings.NewReader(string(buf.Bytes())))
+	r.Header.Set("Content-Length", fmt.Sprintf("%d", buf.Len()))
+	data, err = io.ReadAll(r.Body)
+	if err == nil {
+		log.Println("### new data", string(data))
+	}
 }
 
 // DownloadHandler handles download action of ML model from back-end server
@@ -198,9 +292,9 @@ func UploadHandler(w http.ResponseWriter, r *http.Request) {
 				httpError(w, r, BadRequest, errors.New("unable to get gzip reader"), http.StatusInternalServerError)
 				return
 			}
-			data, err = ioutil.ReadAll(GzipReader{reader, r.Body})
+			data, err = io.ReadAll(GzipReader{reader, r.Body})
 		} else {
-			data, err = ioutil.ReadAll(r.Body)
+			data, err = io.ReadAll(r.Body)
 		}
 		if err != nil {
 			httpError(w, r, BadRequest, errors.New("unable to read body"), http.StatusBadRequest)
