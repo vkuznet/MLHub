5ab82a46624e074a4db56c798900d2d2c590fee7
Valentin Kuznetsov <vkuznet@gmail.com>
2023-04-28 11:21:25 -0400

    New module for errors

---
+++ errors.go
@@ -0,0 +1,10 @@
+package main
+
+const (
+	GenericError        = iota + 100 // generic DBS error
+	DatabaseError                    // 101 database error
+	BadRequest                       // 102 bad request
+	JsonMarshal                      // 103 json.Marshal error
+	MetaDataRecordError              // 104 Meta data record error
+	MetaDataError                    // 105 generic Meta data error
+)
