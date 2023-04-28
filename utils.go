c6918a75d6ed319a48198bb2c748adc3f3552105
Valentin Kuznetsov <vkuznet@gmail.com>
2023-04-28 09:34:47 -0400

    Add InList

--- utils.go
+++ utils.go
@@ -93,3 +93,22 @@ func LogName() string {
 	}
 	return logName
 }
+
+// ListEntry identifies types used by list's generics function
+type ListEntry interface {
+        int | int64 | float64 | string
+}
+
+// InList checks item in a list
+func InList[T ListEntry](a T, list []T) bool {
+        check := 0
+        for _, b := range list {
+                if b == a {
+                        check += 1
+                }
+        }
+        if check != 0 {
+                return true
+        }
+        return false
+}
