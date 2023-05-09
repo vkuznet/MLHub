55fe5955b7283b389b935c58b47b1eee2f8d7ae0
Valentin Kuznetsov <vkuznet@gmail.com>
2023-05-09 16:51:03 -0400

    Code clean-up

--- oauth.go
+++ oauth.go
@@ -11,9 +11,6 @@ import (
 	sessions "github.com/dghubble/sessions"
 )
 
-// sessionStore encodes and decodes session data stored in signed cookies
-var sessionStore = sessions.NewCookieStore[any](sessions.DebugCookieConfig, []byte(sessionSecret), nil)
-
 const (
 	sessionName     = "MLHub-app"
 	sessionSecret   = "signing secret"
@@ -22,11 +19,15 @@ const (
 	sessionToken    = "token"
 )
 
-// issueSession issues a cookie session after successful Github login
+// sessionStore encodes and decodes session data stored in signed cookies
+var sessionStore = sessions.NewCookieStore[any](sessions.DebugCookieConfig, []byte(sessionSecret), nil)
+
+// issueSession issues a cookie session after successful provider login
 func issueSession(provider string) http.Handler {
 	fn := func(w http.ResponseWriter, req *http.Request) {
 		session := sessionStore.New(sessionName)
 		ctx := req.Context()
+		log.Println("### issueSession", provider)
 		if token, err := oauth2Login.TokenFromContext(ctx); err == nil {
 			session.Set(sessionToken, token.AccessToken)
 		} else {
@@ -54,33 +55,6 @@ func issueSession(provider string) http.Handler {
 				log.Println("ERROR: fail to obtain twitter credentials", err)
 			}
 		}
-		if err := session.Save(w); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		http.Redirect(w, req, "/access", http.StatusFound)
-	}
-	return http.HandlerFunc(fn)
-}
-
-// googleSession googles a cookie session after successful Google login
-func googleSession() http.Handler {
-	fn := func(w http.ResponseWriter, req *http.Request) {
-		ctx := req.Context()
-		user, err := google.UserFromContext(ctx)
-		token, _ := oauth2Login.TokenFromContext(ctx)
-		if Config.Verbose > 0 {
-			log.Printf("googleSession\nTOKEN: %+v\nUSER: %+v", token, user)
-		}
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		// 2. Implement a success handler to issue some form of session
-		session := sessionStore.New(sessionName)
-		session.Set(sessionUserKey, user.Id)
-		session.Set(sessionUsername, user.Name)
-		session.Set(sessionToken, token.AccessToken)
 		if err := session.Save(w); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
